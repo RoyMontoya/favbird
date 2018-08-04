6edbdbef7244929ae93027c4ac581bdf3fb49c98
Roy Montoya <roymontoya89@gmail.com>
2018-08-03 17:48:20 -0700

    added navbar

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import NavBar from '../navbar/NavBar'
+import './App.css';
+
+const App = () =>{
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+};
+
+export default App
