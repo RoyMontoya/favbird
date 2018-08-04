4f6de31f2c3c4febba3aa2035119e377a1e172b6
Roy Montoya <roymontoya89@gmail.com>
2018-08-03 18:00:29 -0700

    styled navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import NavBar from '../navbar/NavBar'
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHome)
 
 const App = () =>{
   return (
